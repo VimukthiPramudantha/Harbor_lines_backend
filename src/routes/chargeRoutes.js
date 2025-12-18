39fcfde3bcf54e75f4ef834fa5978e1cb3c50fa5
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2025-12-18 16:12:17 +0530

    define charges routes

---
+++ src/routes/chargeRoutes.js
@@ -0,0 +1,15 @@
+// backend/routes/chargeRoutes.js
+import express from 'express';
+import {
+  createCharge,
+  getAllCharges,
+  updateCharge
+} from '../controllers/chargeController.js';
+
+const router = express.Router();
+
+router.post('/createCharge', createCharge);
+router.get('/getAllCharges', getAllCharges);
+router.put('/updateCharge/:id', updateCharge);
+
+export default router;
