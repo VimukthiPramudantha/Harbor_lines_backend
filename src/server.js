1708739ce37c92b510b04dbe5f176e4157e630b2
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2026-01-29 12:28:17 +0530

    change port to 80

--- src/server.js
+++ src/server.js
@@ -67,8 +67,8 @@ app.use('/api/sales-invoices', salesInvoiceRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB Connected');
-    app.listen(process.env.PORT || 5000, () => {
-      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    app.listen(process.env.PORT || 80, () => {
+      console.log(`Server running on port ${process.env.PORT || 80}`);
     });
   })
   .catch(err => {
